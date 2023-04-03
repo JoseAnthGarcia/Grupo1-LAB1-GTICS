a4e7f7440cfc0984207c32ce4ebd4936830d4f55
Jose-bot-vm <jose.garcia@blueocean.la>
2023-04-03 01:16:54 +0000

    first commit

---
+++ src/main/java/com/example/grupox_lab1_gtics/GrupoXLab1GticsApplication.java
@@ -0,0 +1,13 @@
+package com.example.grupox_lab1_gtics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrupoXLab1GticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrupoXLab1GticsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/grupox_lab1_gtics/GrupoXLab1GticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grupox_lab1_gtics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrupoXLab1GticsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
